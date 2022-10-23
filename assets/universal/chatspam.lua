a24ec03645c26160fb72ae5908328b19d4f58dfd
dragons <102265292+dragonssk@users.noreply.github.com>
2022-10-23 17:50:15 +0000

    Create chatspam.lua

---
+++ assets/universal/chatspam.lua
@@ -0,0 +1,11 @@
+return {
+    '🤖 Calling Reinforcements 😈 Spawn 🤫 On Me ❗',
+    '🤖 Why havent 😂 You joined 😯 gg/phantomforces 💀 Bad player ⚠️',
+    'votekick him!!!!!!! 😠 Vk VK VK VK VOTEKICK HIM!!!!!!!!! 😠 😢 VOTE KICK !!!!! PRESS Y WHY DIDNT U PRESS Y!!!!!! 😭',
+    "Dont Worry ❌👍 I'm  SMURFING‼️",
+    '😦 I Guess ❌ Nobody Can Read 😡',
+    'I NEED ACCESS 🔑🔓 TO RADIANT 🤖📃📃📃 👈 THIS THING CALLED RADIANT SCRlPT, I NEED IT',
+    'before i user radiant: 😔 😭 🙁 after i user radiant: 🤑 😝',
+    " I'm reporting you  to  stylis studios for  CHEeTING! "
+    "god i wish i had radiant..... "
+}
