ae8a98c56487f4c86a4b4355da28d7295ae66f0c
dragons <102265292+dragonssk@users.noreply.github.com>
2022-10-23 18:43:47 +0000

    Update chatspam.lua

--- assets/universal/chatspam.lua
+++ assets/universal/chatspam.lua
@@ -1,10 +1,10 @@
 return {
     '🤖 Calling Reinforcements 😈 Spawn 🤫 On Me ❗',
-    '🤖 Why havent 😂 You Joined 😯 gg/phantomforces 💀 Bad Player ⚠️',
-    'votekick him!!!!!!! 😠 VK VK VK VK VOTEKICK HIM!!!!!!!!! 😠 😢 VOTE KICK !!!!! PRESS Y WHY DIDNT U PRESS Y!!!!!! 😭',
+    '🤖 Why havent 😂 You Joined 😯 gg.phantomforces 💀 Bad Player ⚠️',
+    'votekick him!!!!!!! 😠 VOTEKICK HIM!!!!!!!!! 😠 😢 VOTE KICK !!!!! PRESS Y WHY DIDNT U PRESS Y!!!!!! 😭',
     "Dont Worry ❌👍 I'm  SMURFING‼",
     '😦 I Guess ❌ Nobody Can Read 😡',
-    'I NEED ACCESS 🔑🔓 TO RADIANT 🤖📃📃📃 👈 THIS THING CALLED RADIANT SCRIPT, I NEED IT',
+    'I NEED ACCESS 🔑🔓 TO RADIANT 🤖📃📃📃 👈 THIS THING CALLED RADIANT SCRlPT, I NEED IT',
     'before i user radiant: 😔 😭 🙁 after i user radiant: 🤑 😝',
     "god i wish i had radiant..... ",
 }
